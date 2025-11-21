f0cfca22fa7e985b9c2ef38ec9d7fadf1ce89ec4
SantiagoCalderon10 <u20241221751@usco.edu.co>
2025-11-20 22:56:33 -0500

    js

--- JS/principal.js
+++ JS/principal.js
@@ -1,4 +1,3 @@
-import Swal from "sweetalert2"
 
 const slides = document.querySelectorAll(".hero-slide");
 let index = 0;
@@ -15,11 +14,12 @@ slides[0].classList.add("active");
 // Cambiar cada 5 segundos
 setInterval(changeSlide, 5000);
 
+emailjs.init('Sq8SOy-lmqLTaK7IT')
+
 document
   .getElementById("formulariocontacto")
   .addEventListener("submit", function (event) {
-    event.preventDefault();
-
+    event.preventDefault();  // ← AQUÍ ESTABA EL ERROR
 
     const serviceID = "service_nmzga3q";
     const templateID = "template_q7gcgj3";
@@ -33,11 +33,10 @@ document
         });
       },
       (err) => {
-        btn.value = "Send Email";
         Swal.fire({
           title: "¡Ocurrió un error!",
           icon: "error",
         });
       }
     );
-  });
+  }); 
